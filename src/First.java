ce4f13476c1c20cd3fd585ad9181350e08ee023d
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-07-10 12:07:08 +0530

    first commit

---
+++ src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+        String name="karthik";
+        System.out.println("Hello my name is "+name);
+        int i=10;
+        System.out.println(i);
+    }
+}
